48138c5fc750881b0932d939c97bc251a4c2c4e0
edcartar <edkartar@gmail.com>
2024-12-09 15:56:45 +0100

    hash

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@
 // export default App;
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './pages/Home';
 import ProductDetails from './pages/ProductDetails';
